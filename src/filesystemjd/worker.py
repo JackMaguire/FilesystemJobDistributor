1cb78996d0b89e189eefbfb7ff64140fc85efb0e
Jack Maguire <jackmaguire1444@gmail.com>
2022-06-06 14:22:31 -0500

    switching computers

--- src/filesystemjd/worker.py
+++ src/filesystemjd/worker.py
@@ -1,4 +1,6 @@
-from filesystemjd.signals import *
+from filesystemjd import signals
+from filesystemjd.signals import create_fileprefix, create_wildcard_for_direction
+from filesystemjd.util import create_file
 from pathlib import Path
 import os
 
@@ -22,9 +24,41 @@ class Worker:
         - unique_key (str): some unique (by your filesystem's definition of unique) identifier for this worker. We will create the $signal_dir/worker_$unique_key directory
         """
 
+        # create directory
         self.path = Path( "{}/worker_{}/".format(signal_dir, unique_key) )
         if self.path.exists():
             raise ValueError( "unique_key {} is not unique. Or at least, this path already exists: {}".format( unique_key, str(self.path) ) )
         attempt_to_create_a_directory( self.path )
 
-    def
+        # create "hello" file
+        hello_filename = create_fileprefix(
+            direction = signals.worker_to_head(),
+            task = signals.new_worker_signal(),
+            owner = signals.head_will_delete()
+        ) + ".txt"
+        self.hello_filepath = self.path / hello_filename
+        create_file( self.hello_filepath )
+        if not self.hello_filepath.exists():
+            raise Exception( "failed to create: " + str(self.hello_filepath) )
+        
+
+    def head_has_registered_worker( self ):
+        """
+        Check to see if the hello file still exists. If not, then the head has accepted our handshake
+        """
+        return not self.hello_filepath.exists()
+
+    def wait_for_job( self ):
+        """
+        Returns:
+        - string: the message for the new job
+        - boolean: If the bool is true, then the simulation is over and it is time to shutdown
+        """
+        while True:
+            x = glob.glob(
+                create_wildcard_for_direction( signals.head_to_worker() ),
+                root_dir = self.path
+            )
+            for filename in x:
+                direction, task, owner = parse_filename( filename )
+                contents = 
